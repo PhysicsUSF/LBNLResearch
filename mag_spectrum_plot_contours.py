48864c681a12bf220450fad85d0bb6235db2ec76
AMSTOCKER <sleepyams@gmail.com>
2014-11-13 23:00:15 -0800

    updated mag spectrum plotting

---
+++ mag_spectrum_plot_contours.py
@@ -0,0 +1,219 @@
+import cPickle
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sncosmo as snc
+
+from pprint import pprint
+
+
+TITLE_FONTSIZE = 28
+AXIS_LABEL_FONTSIZE = 24
+TICK_LABEL_FONTSIZE = 16
+INPLOT_LEGEND_FONTSIZE = 20
+LEGEND_FONTSIZE = 15
+
+
+
+def plot_contour(subplot_index, phase, info_dict,
+                 ebv, ebv_pad, rv, rv_pad, ax=None):
+        
+        
+        ret_phase = info_dict['phases'][subplot_index]
+        CHI2 = info_dict['chi2'][subplot_index]
+        CHI2_reduction = info_dict['chi2_reductions'][subplot_index]
+        x = info_dict['x']
+        y = info_dict['y']
+        X = info_dict['X']
+        Y = info_dict['Y']
+        
+        CHI2 = CHI2/CHI2_reduction
+        chi2_min = np.min(CHI2)
+        
+        mindex = np.where(CHI2==chi2_min)
+        mx, my = mindex[1][0], mindex[0][0]
+        
+        print "BEST E(B-V): {}".format(x[mx])
+        print "BEST RV: {}".format(y[my])
+        
+        
+        
+        CDF = 1 - np.exp((-(CHI2-chi2_min))/2)  # calculation cumulative distribution func
+        
+        # find 1-sigma and 2-sigma errors based on confidence
+        maxebv_1sig, maxebv_2sig, minebv_1sig, minebv_2sig = x[mx], x[mx], x[mx], x[mx]
+        maxrv_1sig, maxrv_2sig, minrv_1sig, minrv_2sig = y[my], y[my], y[my], y[my]
+        for i, EBV in enumerate(x):
+                for j, RV in enumerate(y):
+                        _chi2 = CHI2[j,i]-chi2_min
+                        if _chi2<1.00:
+                                maxebv_1sig = np.maximum(maxebv_1sig, EBV)
+                                minebv_1sig = np.minimum(minebv_1sig, EBV)
+                                maxrv_1sig = np.maximum(maxrv_1sig, RV)
+                                minrv_1sig = np.minimum(minrv_1sig, RV)
+                        elif _chi2<4.00:
+                                maxebv_2sig = np.maximum(maxebv_2sig, EBV)
+                                minebv_2sig = np.minimum(minebv_2sig, EBV)
+                                maxrv_2sig = np.maximum(maxrv_2sig, RV)
+                                minrv_2sig = np.minimum(minrv_2sig, RV)
+        
+        #ax.axvline(minebv_1sig, color='r')
+        #ax.axvline(maxebv_1sig, color='r')
+        #ax.axhline(minrv_1sig, color='r')
+        #ax.axhline(maxrv_1sig, color='r')
+        
+        #ax.axvline(minebv_2sig, color='g')
+        #ax.axvline(maxebv_2sig, color='g')
+        #ax.axhline(minrv_2sig, color='g')
+        #ax.axhline(maxrv_2sig, color='g')
+        
+        if ax != None:
+                # plot contours
+                contour_levels = [0.0, 0.683, 0.955, 1.0]
+                plt.contourf(X, Y, 1-CDF, levels=[1-l for l in contour_levels], cmap=mpl.cm.summer, alpha=0.5)
+                C1 = plt.contour(X, Y, CDF, levels=[contour_levels[1]], linewidths=1, colors=['k'], alpha=0.7)
+                
+                # mark minimum
+                plt.scatter(x[mx], y[my], marker='s', facecolors='r')
+                
+                # show results on plot
+                if subplot_index%6==0:
+                        plttext1 = "Phase: {}".format(phase)
+                else:
+                        plttext1 = "{}".format(phase)
+                
+                plttext2 = "$E(B-V)={:.2f}\pm^{{{:.2f}}}_{{{:.2f}}}$" + \
+                           "\n$R_V={:.2f}\pm^{{{:.2f}}}_{{{:.2f}}}$"
+                plttext2 = plttext2.format(x[mx], maxebv_1sig-x[mx], x[mx]-minebv_1sig,
+                                           y[my], maxrv_1sig-y[my], y[my]-minrv_1sig,
+                                           )
+                
+                if phase not in [11.5, 16.5, 18.5, 21.5]:
+                        ax.text(.04, .95, plttext1, size=AXIS_LABEL_FONTSIZE,
+                                horizontalalignment='left',
+                                verticalalignment='top',
+                                transform=ax.transAxes)
+                        ax.text(.04, .85, plttext2, size=INPLOT_LEGEND_FONTSIZE,
+                                horizontalalignment='left',
+                                verticalalignment='top',
+                                transform=ax.transAxes)
+                        ax.axhspan(3.2, (rv+rv_pad), facecolor='k', alpha=0.1)
+                
+                else:
+                        ax.text(.04, .95, plttext1, size=AXIS_LABEL_FONTSIZE,
+                                horizontalalignment='left',
+                                verticalalignment='top',
+                                transform=ax.transAxes)
+                        ax.text(.04, .26, plttext2, size=INPLOT_LEGEND_FONTSIZE,
+                                horizontalalignment='left',
+                                verticalalignment='top',
+                                transform=ax.transAxes)
+                        ax.axhspan(3.42, (rv+rv_pad), facecolor='k', alpha=0.1)
+                        ax.axhspan((rv-rv_pad), 2.75, facecolor='k', alpha=0.1)
+                        
+                        
+                # format subplot...
+                plt.ylim(rv-rv_pad, rv+rv_pad)
+                plt.xlim(ebv-ebv_pad, ebv+ebv_pad)
+                
+                ax.set_yticklabels([])
+                ax2 = ax.twinx()
+                ax2.set_xlim(ebv-ebv_pad, ebv+ebv_pad)
+                ax2.set_ylim(rv-rv_pad, rv+rv_pad)
+                
+                if subplot_index%6 == 5:
+                        ax2.set_ylabel('\n$R_V$', fontsize=AXIS_LABEL_FONTSIZE, labelpad=5)
+                if subplot_index%6 == 0:
+                        ax.set_ylabel('$R_V$', fontsize=AXIS_LABEL_FONTSIZE, labelpad=-2)
+                if subplot_index>=6:
+                        ax.set_xlabel('\n$E(B-V)$', fontsize=AXIS_LABEL_FONTSIZE)
+                
+                # format x labels
+                labels = ax.get_xticks().tolist()
+                labels[0] = labels[-1] = ''
+                ax.set_xticklabels(labels)
+                ax.get_xaxis().set_tick_params(direction='in', pad=-20)
+                
+                # format y labels
+                labels = ax2.get_yticks().tolist()
+                labels[0] = labels[1] = labels[-1] = ''
+                ax2.set_yticklabels(labels)
+                ax2.get_yaxis().set_tick_params(direction='in', pad=-30)
+                
+                plt.setp(ax.get_xticklabels(), fontsize=TICK_LABEL_FONTSIZE)
+                plt.setp(ax2.get_yticklabels(), fontsize=TICK_LABEL_FONTSIZE)
+
+
+
+        
+################################################################################
+
+def main():
+        ebv_guess = 1.05
+        ebv_pad = 0.25
+        
+        rv_guess = 3.0
+        rv_pad = 0.6
+        
+        
+        info_dict = cPickle.load(open("spectra_mag_fit_results_03-44-32-11-14-2014.pkl", 'rb'))
+        phases = info_dict['phases']
+        
+        
+        #### find best fit for all phases
+        CHI2_total = np.sum(info_dict['chi2'], axis=0)
+        CHI2_total_reduction = np.sum(info_dict['chi2_reductions'])
+        
+        CHI2_total /= CHI2_total_reduction
+        
+        chi2_total_min = np.min(CHI2_total)
+        
+        x = info_dict['x']
+        y = info_dict['y']
+        mindex = np.where(CHI2_total==chi2_total_min)
+        mx, my = mindex[1][0], mindex[0][0]
+        
+        
+        # find 1-sigma and 2-sigma errors based on confidence
+        maxebv_1sig, maxebv_2sig, minebv_1sig, minebv_2sig = x[mx], x[mx], x[mx], x[mx]
+        maxrv_1sig, maxrv_2sig, minrv_1sig, minrv_2sig = y[my], y[my], y[my], y[my]
+        for i, EBV in enumerate(x):
+                for j, RV in enumerate(y):
+                        _chi2 = CHI2_total[j,i]-chi2_total_min
+                        if _chi2<1.00:
+                                maxebv_1sig = np.maximum(maxebv_1sig, EBV)
+                                minebv_1sig = np.minimum(minebv_1sig, EBV)
+                                maxrv_1sig = np.maximum(maxrv_1sig, RV)
+                                minrv_1sig = np.minimum(minrv_1sig, RV)
+                        elif _chi2<4.00:
+                                maxebv_2sig = np.maximum(maxebv_2sig, EBV)
+                                minebv_2sig = np.minimum(minebv_2sig, EBV)
+                                maxrv_2sig = np.maximum(maxrv_2sig, RV)
+                                minrv_2sig = np.minimum(minrv_2sig, RV)
+        
+        
+        print "BEST TOTAL E(B-V): {:.4} (+{:.4}/-{:.4})".format(x[mx], maxebv_1sig-x[mx], x[mx]-minebv_1sig)
+        print "BEST TOTAL RV: {:.4} (+{:.4}/-{:.4})".format(y[my], maxrv_1sig-y[my], y[my]-minrv_1sig)
+        print "###################################"
+        
+        ###########################
+        
+        fig = plt.figure()
+        
+        for i, phase in enumerate(phases):
+                print "Plotting phase {} ...".format(phase)
+                
+                ax = plt.subplot(2,6,i+1)
+                
+                plot_contour(i, phase, info_dict, ebv_guess, ebv_pad, rv_guess, rv_pad, ax)
+        
+        
+        fig.subplots_adjust(left=0.04, bottom=0.08, right=0.95, top=0.92, hspace=.06, wspace=.1)
+        fig.suptitle('SN2012CU: $E(B-V)$ vs. $R_V$ per Phase', fontsize=TITLE_FONTSIZE)
+        plt.show()
+
+
+
+
+if __name__=="__main__":
+        main()
---
+++ mag_spectrum_plot_spectra.py
@@ -0,0 +1,233 @@
+'''
+::AUTHOR::
+Andrew Stocker
+
+'''
+import loader as l
+from loader import redden_fm
+from copy import deepcopy
+from pprint import pprint
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cPickle
+
+from scipy.interpolate import interp1d
+
+
+def main():
+        TITLE_FONTSIZE = 28
+        AXIS_LABEL_FONTSIZE = 20
+        TICK_LABEL_FONTSIZE = 16
+        INPLOT_LEGEND_FONTSIZE = 20
+        LEGEND_FONTSIZE = 18
+        
+        info_dict = cPickle.load(open("spectra_mag_fit_results_03-44-32-11-14-2014.pkl", 'rb'))
+        
+        
+        # config
+        ebv_guess = 1.05
+        ebv_pad = 0.25
+        
+        rv_guess = 3.0
+        rv_pad = 0.6
+        
+        steps = 120
+        
+        
+        FEATURES_ACTUAL = [(3425, 3820, 'CaII'), (3900, 4100, 'SiII'), (5640, 5900, 'SiII'),
+                                (6000, 6280, 'SiII'), (8000, 8550, 'CaII')]
+        
+        
+        pristine_12cu = l.get_12cu('fm', ebv=0.024, rv=3.1)[:12]
+        phases = [t[0] for t in pristine_12cu]
+        
+        pristine_11fe = l.interpolate_spectra(phases, l.get_11fe(loadmast=False, loadptf=False))
+        
+        reddened = pristine_12cu
+        
+        
+        def filter_features(features, wave):
+                '''Returns a mask of boolean values the same size as
+                the wave array.  True=wavelength not in features, False=wavelength
+                is in featured.
+                
+                Can be used like:
+                        
+                        mask = filter_features(FEAURES, wave)
+                        wave_no_features = wave[mask]
+                        flux_no_features = flux[mask]
+                '''
+                intersection = np.array([False]*wave.shape[0])
+                for feature in features:
+                        intersection |= ((wave>feature[0])&(wave<feature[1]))
+                
+                return ~intersection
+        
+        
+        def perphase_fit():
+                
+                fig = plt.figure()
+                ax = fig.gca()
+                
+                best_rvs = []
+                best_ebvs = []
+                best_avs = []
+                chi2s = []
+                min_chi2s = []
+                
+                for phase_index in xrange(len(phases)):
+                        ref = pristine_11fe[phase_index]
+                        red = reddened[phase_index]
+                        
+                        CHI2 = info_dict['chi2'][phase_index]
+                        CHI2_reduction = info_dict['chi2_reductions'][phase_index]
+                        x = info_dict['x']
+                        y = info_dict['y']
+                        
+                        CHI2 = CHI2/CHI2_reduction
+                        chi2_min = np.min(CHI2)
+                        
+                        
+                        mindex = np.where(CHI2==chi2_min)
+                        mx, my = mindex[1][0], mindex[0][0]
+                        
+                        print "\t", mindex
+                        print "\t", y[my], x[mx]
+                        
+                        best_rvs.append(y[my])
+                        best_ebvs.append(x[mx])
+                        best_avs.append(x[mx]*y[my])
+                        
+                        
+                        
+                        #### plot best fit ###
+                        
+                        mask = filter_features(FEATURES_ACTUAL, ref[1].wave)
+                        
+                        interpolated_red = interp1d(red[1].wave, red[1].flux)(ref[1].wave)
+                        unred_flux = redden_fm(ref[1].wave, interpolated_red, x[mx], y[my])
+                        
+                        
+                        # normalization
+                        unred_flux_avg_mag = 2.5*np.log10(np.average(unred_flux))
+                        unred_mag = 2.5*np.log10(unred_flux)
+                        unred_mag_norm = unred_mag - unred_flux_avg_mag
+                        
+                        ref_flux_avg_mag = 2.5*np.log10(np.average(ref[1].flux))
+                        ref_mag = 2.5*np.log10(ref[1].flux)
+                        ref_mag_norm = ref_mag - ref_flux_avg_mag
+                        
+                        adjust = 1.3*phase_index
+                        
+                        
+                        
+                        #mag_wave = (ref[1].wave)[mask]
+                        #plt.plot(mag_wave, unred_mag_norm[mask]-adjust, 'r.', ms=3)
+                        #plt.plot(mag_wave, ref_mag_norm[mask]-adjust, 'g.', ms=3)
+                        
+                        
+                        # plot unfiltered spectrum used in fit
+                        mag_wave = (ref[1].wave)[mask]
+                        
+                        unred_nanmask = ~np.isnan(unred_mag_norm[mask])
+                        unred_mag_norm_u = unred_mag_norm[mask][unred_nanmask]
+                        unred_mag_wave_u = mag_wave[unred_nanmask]
+                        
+                        ref_nanmask = ~np.isnan(ref_mag_norm[mask])
+                        ref_mag_norm_u = ref_mag_norm[mask][ref_nanmask]
+                        ref_mag_wave_u = mag_wave[ref_nanmask]
+                        
+                        plt.plot(unred_mag_wave_u, unred_mag_norm_u-adjust, 'r.', ms=4)
+                        plt.plot(ref_mag_wave_u, ref_mag_norm_u-adjust, 'g.', ms=4)
+                        
+                        
+                        # plot blocked out region of spectrum
+                        mag_wave = (ref[1].wave)[~mask]
+                        
+                        unred_nanmask = ~np.isnan(unred_mag_norm[~mask])
+                        unred_mag_norm_f = unred_mag_norm[~mask][unred_nanmask]
+                        unred_mag_wave_f = mag_wave[unred_nanmask]
+                        
+                        ref_nanmask = ~np.isnan(ref_mag_norm[~mask])
+                        ref_mag_norm_f = ref_mag_norm[~mask][ref_nanmask]
+                        ref_mag_wave_f = mag_wave[ref_nanmask]
+                        
+                        plt.plot(unred_mag_wave_f, unred_mag_norm_f-adjust, 'k.', ms=2)
+                        plt.plot(ref_mag_wave_f, ref_mag_norm_f-adjust, 'k.', ms=2)
+                        
+                        
+                        # plot phase label
+                        #ty = (ref_mag_norm_u-adjust)[0]
+                        ty = (ref_mag_norm-adjust)[0]
+                        if phase_index==0:
+                                plt.text(3250, ty, "Phase:\n{}".format(ref[0]), horizontalalignment='right',
+                                         color='k', fontsize=INPLOT_LEGEND_FONTSIZE)
+                        else:
+                                plt.text(3250, ty, str(ref[0]), horizontalalignment='right',
+                                         color='k', fontsize=INPLOT_LEGEND_FONTSIZE)
+                
+                
+                
+                # gray bands at blocked out features
+                for feature in FEATURES_ACTUAL:
+                        plt.axvspan(feature[0], feature[1], facecolor='k', alpha=0.1)
+                        plt.text((feature[0]+feature[1])/2, -27.5, '${}$'.format(feature[2]),
+                                horizontalalignment='center',
+                                #transform=ax.transAxes,
+                                fontsize=AXIS_LABEL_FONTSIZE)
+                
+                
+                # plot table with rv, ebv, and av values
+                r = lambda n: round(n, 3)
+                values = [phases, map(r,best_rvs), map(r,best_ebvs), map(r,best_avs)]
+                alpha = 1 - 0.1
+                cellColours = np.array([[[alpha,alpha,alpha,1]]*12] + [[[1,1,1,1]]*12]*3 )
+                rowColours = np.array([[alpha,alpha,alpha,1]] + [[1,1,1,1]]*3 )
+                
+                table = plt.table(cellText=values,
+                                  rowLabels=['PHASE', '$R_V$', '$E(B-V)$', '$A_V$'],
+                                  cellColours=cellColours,
+                                  rowColours=rowColours,
+                                  loc='bottom')
+                
+                table.set_fontsize(14)                  
+                
+                
+                ### format plot ###
+                
+                # y-axis
+                #plt.yticks([])
+                plt.ylabel('$-mag_{\lambda}$ (adjusted)', fontsize=AXIS_LABEL_FONTSIZE, labelpad=10)
+                #plt.ylim(-45,-27)
+                
+                # x-axis
+                plt.xlim(2800,10000)
+                labels = ax.get_xticks().tolist()
+                labels = map(int, labels)
+                labels[-1] = ''
+                ax.set_xticklabels(labels)
+                ax.get_xaxis().set_tick_params(direction='in', pad=-20)
+                plt.setp(ax.get_xticklabels(), fontsize=TICK_LABEL_FONTSIZE)
+                ax.text(.5,.045,'Wavelength ($\AA$)',
+                        horizontalalignment='center',
+                        transform=ax.transAxes,
+                        fontsize=AXIS_LABEL_FONTSIZE)
+                
+                # legend
+                plt.plot([], [], 'r.', ms=18, label='SN2012cu (Unreddened)')
+                plt.plot([], [], 'g.', ms=18, label='SN2011fe')
+                plt.legend(prop={'size':LEGEND_FONTSIZE})
+                
+                plt.subplots_adjust(left=0.07, bottom=0.13, right=0.95, top=0.92)
+                
+                plt.title('Spectrum Comparison: SN2012cu vs. SN2011fe', fontsize=TITLE_FONTSIZE)
+                plt.show()
+                
+                
+        perphase_fit()
+
+
+
+if __name__=="__main__":
+        main()
