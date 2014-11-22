167e3ccd0ae93fd7af59c9c7745583f614f9dfb1
highredshift <xhuang22@usfca.edu>
2014-11-22 11:53:27 -0800

    made lots of changes. the current version works well with mag_spectrum_plot_excess.py. still there are issues with the excess fit plots.

--- mag_spectrum_fitting.py
+++ mag_spectrum_fitting.py
@@ -11,14 +11,14 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 import cPickle
-from time import localtime, strftime
+from time import localtime, strftime, gmtime
 
 from scipy.interpolate import interp1d
 
 
 
 def main():
-        
+    
         # config
         ebv_guess = 1.05
         ebv_pad = 0.25
@@ -26,12 +26,16 @@ def main():
         rv_guess = 3.0
         rv_pad = 0.6
         
-        steps = 120
+        steps = 21
+        #steps = 120
         
         
         FEATURES_ACTUAL = [(3425, 3820, 'CaII'), (3900, 4100, 'SiII'), (5640, 5900, 'SiII'),
-                                (6000, 6280, 'SiII'), (8000, 8550, 'CaII')]
-        
+                               (6000, 6280, 'SiII'), (8000, 8550, 'CaII')]
+
+#FEATURES_ACTUAL = [(300, 310, 'Null'),]  # does NOT block anything.
+
+
         # Use an empty list of features to fit for the entire spectrum:
         #FEATURES_ACTUAL = []
         
@@ -39,6 +43,7 @@ def main():
         # load spectra, interpolate 11fe to 12cu phases (only first 12)
         pristine_12cu = l.get_12cu('fm', ebv=0.024, rv=3.1)[:12]
         phases = [t[0] for t in pristine_12cu]
+        
         
         pristine_11fe = l.interpolate_spectra(phases, l.get_11fe(loadmast=False, loadptf=False))
         
@@ -55,34 +60,37 @@ def main():
         
         ########################
         ### helper functions ###
+
+
+        def filter_features(features, wave):
+            '''Returns a mask of boolean values the same size as
+                the wave array.  True=wavelength not in features, False=wavelength
+                is in featured.
         
+            Can be used like:
+            
+            mask = filter_features(FEAURES, wave)
+            wave_no_features = wave[mask]
+            flux_no_features = flux[mask]
+                '''
+            intersection = np.array([False]*wave.shape[0])
+            for feature in features:
+                intersection |= ((wave>feature[0])&(wave<feature[1]))
+                        
+            return ~intersection
+
+
+
         def log(msg=""):
                 # attach time stamp to print statements
                 print "[{}] {}".format(strftime("%Y-%m-%d %H:%M:%S", localtime()), msg)
         
         
-        def filter_features(features, wave):
-                '''Returns a mask of boolean values the same size as
-                the wave array.  True=wavelength not in features, False=wavelength
-                is in featured.
-                
-                Can be used like:
-                        
-                        mask = filter_features(FEAURES, wave)
-                        wave_no_features = wave[mask]
-                        flux_no_features = flux[mask]
-                '''
-                intersection = np.array([False]*wave.shape[0])
-                for feature in features:
-                        intersection |= ((wave>feature[0])&(wave<feature[1]))
-                
-                return ~intersection
-        
         
         def flux2mag(flux, var=None, calibration_err=None):
                 mag_var = None
                 calibration_err_mag = None
-                
+
                 mag = -2.5*np.log10(flux)
                 
                 # calculate magnitude error
@@ -120,8 +128,17 @@ def main():
                 min_chi2s = []
                 chi2_reductions = []
                 
+                #V_band = [(5300., 5500., 'Vband')]
+
+                del_lamb = 1.
+                V_wave = 5413.5  # see my mag_spectrum_plot_excess.py
+                band_steps = 1200
+                V_band_range = np.linspace(V_wave - del_lamb*band_steps/2., V_wave + del_lamb*band_steps/2., band_steps+1)
+
+                
                 for phase_index in xrange(len(phases)):
-                        
+                    
+                    
                         ref = pristine_11fe[phase_index]
                         red = reddened[phase_index]
                         
@@ -139,34 +156,62 @@ def main():
                         ref_flux = ref[1].flux
                         ref_flux_avg = np.average(ref_flux)
                         ref_flux_avg_mag = -2.5*np.log10(ref_flux_avg)
+                        #ref_flux_avg_mag = -2.5*np.log10(ref_flux[np.abs(ref_wave - 5413.5).argmin()])  #normalize with single-wavelength V band magnitude.
+
+                        ref_interp = interp1d(ref_wave, ref_flux)
+
+
                         ref_var = ref[1].error
                         ref_calibration_error = ref[2]
                         
                         # convert flux, variance, and calibration error to magnitude space
+
                         ref_mag, ref_mag_var, ref_calibration_error_mag \
                         = flux2mag(ref_flux, ref_var, ref_calibration_error)
                         
                         # normalize for later use
-                        ref_mag_norm = ref_mag - ref_flux_avg_mag
-                        
+                        #Vband_mask = filter_features(V_band, ref_wave) # Not the most efficient way of doing things, but this statement is here because ref_wave is inside the for loop -- also inefficient. Should fix this.
+                        ref_V_mag = -2.5*np.log10(ref_interp(V_band_range).mean())
+
+#ref_flux_V_mag = -2.5*np.log10(np.average(ref_flux[Vband_mask]))
+
+                        ref_mag_norm = ref_mag - ref_flux_avg_mag  #ref_V_mag
+    
                         # get mask for nan-values
                         nanmask_ref = ~np.isnan(ref_mag_norm[mask])
                         
+                        #fig = plt.figure()
+                        #plt.plot(ref_wave[mask], ref_flux[mask], 'ro')
+                        #plt.plot(ref_wave, ref_flux)
+                        #plt.show()
+                        
                         # 12cu/reddened 11fe
                         red_wave = red[1].wave
-                        red_interp_flux = interp1d(red_wave, red[1].flux)(ref_wave)
-                        
+#     red_flux = interp1d(red_wave, red[1].flux)(ref_wave)
+
+                        red_interp = interp1d(red_wave, red[1].flux)
+    
+                        red_flux = red_interp(ref_wave)
+
+
+
                         ## test ######################################################################
                         #red_noisy_flux = (1 + 0.05*np.random.randn(red[1].flux.shape[0]) )*red[1].flux
-                        #red_interp_flux = interp1d(red_wave, red_noisy_flux)(ref_wave)
+                        #red_flux = interp1d(red_wave, red_noisy_flux)(ref_wave)
                         ##############################################################################
                         
                         red_interp_var  = interp1d(red_wave, red[1].error)(ref_wave)
                         red_calibration_error = red[2]
                         
                         # convert flux, variance, and calibration error to magnitude space
+
+                        if (red_flux <= 0).any():
+                            print "some red_flux values are not positive:", red_flux[np.where(red_flux <= 0)]
+                            print "These values will be rejected below as nan for the log."
+                            print "(But it's better to deal with the non-pos values before taking the log.  Something to deal with later.)"
+
                         red_interp_mag, red_interp_mag_var, red_calibration_error_mag \
-                        = flux2mag(red_interp_flux, red_interp_var, red_calibration_error)
+                        = flux2mag(red_flux, red_interp_var, red_calibration_error)
                         
                         # get mask for nanvalues
                         nanmask_red_interp = ~np.isnan(red_interp_mag[mask])
@@ -260,23 +305,41 @@ def main():
                                 for k, RV in enumerate(y):
                                         
                                         # unredden the reddened spectrum, convert to mag
-                                        unred_flux = redden_fm(ref_wave, red_interp_flux, EBV, RV)
+                                        unred_flux = redden_fm(ref_wave, red_flux, EBV, RV)
                                         unred_mag = flux2mag(unred_flux)
+                                        
+                                        unred_interp = interp1d(ref_wave, unred_flux)
                                         
                                         # normalization
                                         unred_flux_avg_mag = -2.5*np.log10(np.average(unred_flux))
-                                        unred_mag_norm = unred_mag - unred_flux_avg_mag
                                         
+                                        unred_V_mag = -2.5*np.log10(unred_interp(V_band_range).mean())
+                                        #unred_flux_avg_mag = -2.5*np.log10(unred_flux[np.abs(ref_wave - 5413.5).argmin()])  #normalize with single-wavelength V band magnitude.
+                                        #unred_flux_V_mag = -2.5*np.log10(np.average(unred_flux[Vband_mask]))
+                                        #unred_mag_norm = unred_mag - unred_flux_V_mag
+
+                                        unred_mag_norm = unred_mag - unred_flux_avg_mag  #unred_V_mag
+    
                                         # this is unreddened 12cu mag - pristine 11fe mag
                                         delta = unred_mag_norm[mask]-ref_mag_norm[mask]
-                                        
+                                        tmp_wave = ref_wave[mask]
                                         # convert to vector from array and filter nan-values
                                         delta = np.matrix(delta[nanmask])
+                                        
+                                        #delta_array = np.squeeze(np.asarray(delta))  # converting 1D matrix to 1D array.
+                                        ## ----->I shoudl fix ylim<-------------------
+                                        #fig = plt.figure()
+                                        #plt.plot(tmp_wave[nanmask], delta_array, 'ro')
+                                       
                                         
                                         # The original equation is delta.T * C_inv * delta, but delta
                                         #  is already a row vector in numpy so it is the other way around.
                                         CHI2[k,j] = (delta * TMP_C_total_inv * delta.T)[0,0]
                                         
+                        
+                        #plt.show()
+                        
+                        
                         #################################################
                         
                         ### report/save results
@@ -301,8 +364,9 @@ def main():
                 pprint( zip(phases, best_rvs, best_ebvs, best_avs, min_chi2s) )
                 
                 # save results
-                filename = "spectra_mag_fit_results_{}.pkl".format(strftime("%H-%M-%S-%m-%d-%Y", gmtime()))
-                
+                #                filename = "spectra_mag_fit_results_{}.pkl".format(strftime("%H-%M-%S-%m-%d-%Y", gmtime()))
+
+                filename = "spectra_mag_fit_results_FILTERED.pkl"
                 cPickle.dump({'phases': phases, 'rv': best_rvs, 'ebv': best_ebvs, 'av': best_avs,
                                 'chi2': chi2s, 'chi2_reductions': chi2_reductions, 'steps': steps,
                                 'x': x, 'y': y, 'X': X, 'Y': Y},
