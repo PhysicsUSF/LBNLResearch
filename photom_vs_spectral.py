45caf89ce58f5a2ac8dffd13c627cd82e4ed7557
highredshift <xhuang22@usfca.edu>
2014-12-07 01:29:50 -0800

    small change

--- photom_vs_spectral.py
+++ photom_vs_spectral.py
@@ -66,7 +66,7 @@ This program will fit RV based on color excess of 2012cu
 
 '''
 import argparse
-
+from copy import deepcopy
 
 import loader as l
 import matplotlib as mpl
@@ -89,8 +89,8 @@ from mag_spectrum_fitting import ABmag_nu
 c = 3e18  # speed of light in A/sec.
 
 ### VARS ###
-STEPS = 100
-#N_BUCKETS = 20
+
+
 
 EBV_GUESS = 1.1
 EBV_PAD = .3
@@ -104,6 +104,7 @@ TICK_LABEL_FONTSIZE = 16
 INPLOT_LEGEND_FONTSIZE = 20
 LEGEND_FONTSIZE = 15
 
+PLOTS_PER_ROW = 6
 
 '''This function needs to be adapted for my way of doing things:
     
@@ -117,27 +118,34 @@ LEGEND_FONTSIZE = 15
 
 
 
-def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
+#def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
+def plot_phase_excesses(name, sn11fe_colors, sn12cu_colors, filter_eff_waves, SN12CU_CHISQ_DATA, filters, red_law, phases, pmin, pmax):
     
     
     print "Plotting excesses of",name," with best fit from contour..."
     
-    prefix = zp['prefix']
-    phases = [d['phase'] for d in SN12CU_CHISQ_DATA]
-    ref = loader(phases, filters, zp)
-    filter_eff_waves = [snc.get_bandpass(prefix+f).wave_eff for f in filters]
+    #    prefix = zp['prefix']
+    #   phases = [d['phase'] for d in SN12CU_CHISQ_DATA]
+    #   ref = loader(phases, filters, zp)
+    #filter_eff_waves = [snc.get_bandpass(prefix+f).wave_eff for f in filters]
 
     # get 11fe synthetic photometry at BMAX, get ref sn color excesses at BMAX
-    sn11fe = l.interpolate_spectra(phases, l.get_11fe())
+    #sn11fe = l.interpolate_spectra(phases, l.get_11fe())
     
     numrows = (len(phases)-1)//PLOTS_PER_ROW + 1
-    pmin, pmax = np.min(phases), np.max(phases)
+    #pmin, pmax = np.min(phases), np.max(phases)
     
-    for i, d, sn11fe_phase in izip(xrange(len(SN12CU_CHISQ_DATA)), SN12CU_CHISQ_DATA, sn11fe):
-        phase = d['phase']
-        print "Plotting phase {} ...".format(phase)
-        ax = plt.subplot(numrows, PLOTS_PER_ROW, i+1)
+    #    for i, d, sn11fe_phase in izip(xrange(len(SN12CU_CHISQ_DATA)), SN12CU_CHISQ_DATA, sn11fe):
+    for i, d, phase in izip(xrange(len(SN12CU_CHISQ_DATA)), SN12CU_CHISQ_DATA, phases):
+        #phase = d['phase']
         
+        print "Plotting phase {} ...".format(phase)
+        #exit(1)
+            
+            
+            #ax = plt.subplot(numrows, PLOTS_PER_ROW, i+1)
+        ax = plt.subplot(111)
+            
         # calculate sn11fe band magnitudes.  Note since the method bandflux() returns the mag and the error, I have added [0] to select the total flux.  -XH
         #        sn11fe_mags = {f : -2.5*np.log10(sn11fe_phase[1].bandflux(prefix+f)[0]/zp[f])
         #               for f in filters}
@@ -151,18 +159,25 @@ def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
         #        sn11fe_colors = [sn11fe_mags['V']-sn11fe_mags[f] for f in filters]
         
         # make list of colors of reference supernova for given phase index i.  Again each ref[i][f] is an array of two number: mag and flux error converted to mag (via -2.5*log(flux error), which is not the right way); I use [0] to select the mag for now.  -XH
-        ref_colors = [ref[i][f][0] for f in filters]
+#        print 'sn11fe_colors', sn11fe_colors
+#        print 'sn11fe_colors[0]', sn11fe_colors[0]
+
+
+        
+        ref_colors = [sn11fe_colors[i][f] for f in filters]
+        SN_colors = [sn12cu_colors[i][f] for f in filters]
+
 #        print 'ref_colors', ref_colors
 #        exit(1)
 
         # get colors excess of reference supernova compared for sn11fe
-        phase_excesses = np.array(ref_colors)-np.array(sn11fe_colors)
+        phase_excesses = np.array(SN_colors)-np.array(ref_colors)
   
         
         # convert effective wavelengths to inverse microns then plot
-        eff_waves_inv = (10000./np.array(filter_eff_waves))
+        #eff_waves_inv = 10000./filter_eff_waves
         mfc_color = plt.cm.cool((phase-pmin)/(pmax-pmin))    
-        plt.plot(eff_waves_inv, phase_excesses, 's', color=mfc_color,
+        plt.plot(filter_eff_waves, phase_excesses, 's', color=mfc_color,
                  ms=8, mec='none', mfc=mfc_color, alpha=0.8)
 #        print 'eff_waves_inv', eff_waves_inv
 #        print 'phase_excesses', phase_excesses
@@ -173,21 +188,23 @@ def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
         linestyle = '--'
         
         x = np.arange(3000,10000,10)
-        xinv = 10000./x
+        #xinv = 10000./x
         red_curve = red_law(x, np.zeros(x.shape), -d['BEST_EBV'], d['BEST_RV'], return_excess=True)
-        plt.plot(xinv, red_curve, 'k'+linestyle)
+        plt.plot(x, red_curve, 'k'+linestyle)
         slo, shi = plot_snake(ax, x, red_curve, red_law, d['x'], d['y'], d['CDF'])
 
-        
-        
+        ebv_spect = 1.0
+        rv_spect = 2.83
+        red_curve_spect = red_law(x, np.zeros(x.shape), -ebv_spect, rv_spect, return_excess=True)
+        plt.plot(x, red_curve_spect, 'r-')
         
         # TEST #####################
-        dd = SN12CU_CHISQ_DATA_PL[i]
-        test_red_curve = redden_pl2(x, np.zeros(x.shape), -dd['BEST_EBV'], dd['BEST_RV'], return_excess=True)
-        plt.plot(xinv, test_red_curve, 'r-')
-        AV = dd['BEST_EBV']*dd['BEST_RV']
-        P = np.log((1/dd['BEST_RV'])+1)/np.log(0.8)
-        W.writerow([phase, AV, P])
+#        dd = SN12CU_CHISQ_DATA_PL[i]
+#        test_red_curve = redden_pl2(x, np.zeros(x.shape), -dd['BEST_EBV'], dd['BEST_RV'], return_excess=True)
+#        plt.plot(xinv, test_red_curve, 'r-')
+#        AV = dd['BEST_EBV']*dd['BEST_RV']
+#        P = np.log((1/dd['BEST_RV'])+1)/np.log(0.8)
+#        W.writerow([phase, AV, P])
         ############################
 
 
@@ -212,7 +229,7 @@ def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
                                  d['BEST_AV'], d['AV_1SIG'][1]-d['BEST_AV'], d['BEST_AV']-d['AV_1SIG'][0]
                                  )
         
-        ax.text(.95, .98, plttext, size=INPLOT_LEGEND_FONTSIZE,
+        ax.text(.95, .2, plttext, size=INPLOT_LEGEND_FONTSIZE,
                 horizontalalignment='right',
                 verticalalignment='top',
                 transform=ax.transAxes)
@@ -224,7 +241,7 @@ def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
         else:
             ax.set_title('{}'.format(phase), fontsize=AXIS_LABEL_FONTSIZE)
     
-        plt.xlim(1.0, 3.0)
+        plt.xlim(3000, 10000)
         plt.ylim(-3.0, 2.0)
         
         labels = ax.get_yticks().tolist()
@@ -237,13 +254,38 @@ def plot_phase_excesses(name, loader, SN12CU_CHISQ_DATA, red_law, filters, zp):
         
         plt.setp(ax.get_xticklabels(), fontsize=TICK_LABEL_FONTSIZE)
         plt.setp(ax.get_yticklabels(), fontsize=TICK_LABEL_FONTSIZE)
-    
+
 
 
 
     # TEST #####################
-    F.close()
+#F.close()
     ############################
+
+
+def plot_snake(ax, wave, init, red_law, x, y, CDF, plot2sig=False):
+    snake_hi_1sig = deepcopy(init)
+    snake_lo_1sig = deepcopy(init)
+    if plot2sig:
+        snake_hi_2sig = deepcopy(init)
+        snake_lo_2sig = deepcopy(init)
+    
+    for i, EBV in enumerate(x):
+        for j, RV in enumerate(y):
+            if CDF[j,i]<0.683:
+                red_curve = red_law(wave, np.zeros(wave.shape), -EBV, RV, return_excess=True)
+                snake_hi_1sig = np.maximum(snake_hi_1sig, red_curve)
+                snake_lo_1sig = np.minimum(snake_lo_1sig, red_curve)
+            elif plot2sig and CDF[j,i]<0.955:
+                red_curve = red_law(wave, np.zeros(wave.shape), -EBV, RV, return_excess=True)
+                snake_hi_2sig = np.maximum(snake_hi_2sig, red_curve)
+                snake_lo_2sig = np.minimum(snake_lo_2sig, red_curve)
+    
+    ax.fill_between(wave, snake_lo_1sig, snake_hi_1sig, facecolor='black', alpha=0.3)
+    if plot2sig:
+        ax.fill_between(wave, snake_lo_2sig, snake_hi_2sig, facecolor='black', alpha=0.1)
+    
+    return interp1d(wave, snake_lo_1sig), interp1d(wave, snake_hi_1sig)
 
 
 
@@ -251,17 +293,24 @@ if __name__ == "__main__":
 
     '''
     
-    python photom_vs_spectral.py -N_BUCKETS 20
+    python photom_vs_spectral.py -N_BUCKETS 20 -RV_STEPS 41 -EBV_STEPS 41
     
     
     '''
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-N_BUCKETS', type = int)
+    parser.add_argument('-RV_STEPS', type = int)
+    parser.add_argument('-EBV_STEPS', type = int)
+
+
 
     args = parser.parse_args()
     print 'args', args
-    N_BUCKETS = args.N_BUCKETS
+    RV_STEPS = args.N_BUCKETS
+    EBV_STEPS = args.RV_STEPS
+    N_BUCKETS = args.EBV_STEPS
+    
     hi_wave = 9700.
     lo_wave = 3300.
     
@@ -298,7 +347,7 @@ if __name__ == "__main__":
 #    plt.plot(np.array(filter_eff_waves), sn12cu_only_mags, 's', ms=8, mec='none')
 #    plt.plot(ref_wave, mag_12cu, 'k.')
 #    
-    plt.show()
+
 #exit(1)
     
     for i, phase, sn12cu_phase in izip(xrange(1), phases, sn12cu):
@@ -315,7 +364,7 @@ if __name__ == "__main__":
     plt.plot(filter_eff_waves, sn12cu_only_mags, 's', ms=8, mec='none')
     plt.plot(ref_wave, mag_12cu, 'k.')
 
-    plt.show()
+
 
 
 
@@ -327,8 +376,14 @@ if __name__ == "__main__":
         ax = plt.subplot(111)  # ax = plt.subplot(2,6,i+1)
         #                print 'sn12cu_excess[i].shape', sn12cu_excess[i].shape
         #                exit(1)
+        
+        
+        ####**** NOTE: plot_contour() is called twic here.   Fix this *****####
+        
+        
+        ## plot_contour() is where chi2 is calculated.
         plot_contour(i, phase, redden_fm, sn12cu_excess[i], filter_eff_waves,
-                     EBV_GUESS, EBV_PAD, RV_GUESS, RV_PAD, STEPS, ax)
+                     EBV_GUESS, EBV_PAD, RV_GUESS, RV_PAD, RV_STEPS, EBV_STEPS, ax)
 
 
         x, y, CDF, \
@@ -337,7 +392,7 @@ if __name__ == "__main__":
             rv_1sig, rv_2sig, \
             av_1sig, av_2sig = plot_contour(i, phase, redden_fm, sn12cu_excess[i],
                                             filter_eff_waves, EBV_GUESS,
-                                            EBV_PAD, RV_GUESS, RV_PAD, STEPS
+                                            EBV_PAD, RV_GUESS, RV_PAD, RV_STEPS, EBV_STEPS
                                             )
         
         SN12CU_CHISQ_DATA.append({'phase'   : phase,
@@ -356,75 +411,16 @@ if __name__ == "__main__":
                                  })
 
 
-
-
-
-
-
     fig.subplots_adjust(left=0.04, bottom=0.08, right=0.95, top=0.92, hspace=.06, wspace=.1)
     fig.suptitle('SN2012CU: $E(B-V)$ vs. $R_V$ Contour Plot per Phase', fontsize=TITLE_FONTSIZE)
+
+    fig = plt.figure(figsize = (20, 12))
+    plot_phase_excesses('SN2012CU', sn11fe_colors, sn12cu_colors, filter_eff_waves, SN12CU_CHISQ_DATA, filters_bucket, redden_fm, phases, pmin, pmax)
+
     plt.show()
 
 
 
 
-#eff_waves_inv = (10000./np.array(filter_eff_waves))
-#mfc_color = plt.cm.cool((phase-pmin)/(pmax-pmin))
-#   plt.plot(eff_waves_inv, phase_excesses, 's', color=mfc_color,
-#             ms=8, mec='none', mfc=mfc_color, alpha=0.8)
-#        print 'eff_waves_inv', eff_waves_inv
-
-
-#    filters_bucket, zp_bucket = l.generate_buckets(3300, 9700, N_BUCKETS, inverse_microns=True)
-#    
-#    #        print 'filters_bucket', filters_bucket
-#    #        print 'zp_bucket', zp_bucket
-#    #        exit(1)
-#    
-#    filter_eff_waves = np.array([snc.get_bandpass(zp_bucket['prefix']+f).wave_eff for f in filters_bucket])
-#
-#    for f in filters_bucket:
-#        band_wave = snc.get_bandpass(zp_bucket['prefix']+f).wave
-#        band_wave_grad = np.gradient(band_wave)
-#        band_trans = np.array([snc.get_bandpass(zp_bucket['prefix']+f).trans for f in filters_bucket])
-##        weights = band_trans * band_wave_grad
-##            return np.sum(self.wave * weights) / np.sum(weights)
-#
-#    phases =   _excess_X(filters_bucket, zp_bucket)
-
-
-
-## Plotting excess fit
-#    plot_phase_excesses('SN2012CU', load_12cu_colors, RED_LAW, filters_bucket, zp_bucket)
-#    
-#    # format figure
-#    fig.suptitle('SN2012CU: Color Excess Per Phase', fontsize=TITLE_FONTSIZE)
-#    
-#    fig.text(0.5, .05, 'Inverse Wavelength ($1 / \mu m$)',
-#             fontsize=AXIS_LABEL_FONTSIZE, horizontalalignment='center')
-#        
-#             p1, = plt.plot(np.array([]), np.array([]), 'k--')
-#             p2, = plt.plot(np.array([]), np.array([]), 'r-')
-#             fig.legend([p1, p2], ['Fitzpatrick-Massa 1999*', 'Power-Law (Goobar 2008)'],
-#                        loc=1, bbox_to_anchor=(0, 0, .97, .99), ncol=2, prop={'size':LEGEND_FONTSIZE})
-#             
-#             fig.subplots_adjust(left=0.06, bottom=0.1, right=0.94, top=0.90, wspace=0.2, hspace=0.2)
-#             plt.show()
-#    plot_phase_excesses('SN2012CU', load_12cu_colors, RED_LAW, filters_bucket, zp_bucket)
-#    
-#    # format figure
-#    fig.suptitle('SN2012CU: Color Excess Per Phase', fontsize=TITLE_FONTSIZE)
-#    
-#    fig.text(0.5, .05, 'Inverse Wavelength ($1 / \mu m$)',
-#                fontsize=AXIS_LABEL_FONTSIZE, horizontalalignment='center')
-#    
-#    p1, = plt.plot(np.array([]), np.array([]), 'k--')
-#    p2, = plt.plot(np.array([]), np.array([]), 'r-')
-#    fig.legend([p1, p2], ['Fitzpatrick-Massa 1999*', 'Power-Law (Goobar 2008)'],
-#               loc=1, bbox_to_anchor=(0, 0, .97, .99), ncol=2, prop={'size':LEGEND_FONTSIZE})
-#    
-#    fig.subplots_adjust(left=0.06, bottom=0.1, right=0.94, top=0.90, wspace=0.2, hspace=0.2)
-#    plt.show()
-#
 
 
