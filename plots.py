73e32d3d9807ac8ea6478210cb98a6dbf89a9bd9
highredshift <xhuang22@usfca.edu>
2014-12-23 13:03:37 -0800

    making better plots

--- plots.py
+++ plots.py
@@ -8,6 +8,10 @@ import loader as l
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.ticker import *
+#Formatter
+
+
 import sncosmo as snc
 
 from itertools import izip
@@ -70,10 +74,10 @@ def plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt):
  
     contour_levels = [0.0, 0.683, 0.955, 1.0]
 
-    fig = plt.figure(figsize = (20, 12))
+    fig = plt.figure(figsize = (24, 15))
 
 
-    for i, phase, d in zip(range(len(SN12CU_CHISQ_DATA)), phases, SN12CU_CHISQ_DATA):
+    for phase_index, phase, d in zip(range(len(SN12CU_CHISQ_DATA)), phases, SN12CU_CHISQ_DATA):
 
     ## Need to think about how to do contour plots -- basically what Zach and I went through in Starbucks in October.
     ## Don't delete below yet.  There is useful code below about the plotting styles. 
@@ -81,29 +85,19 @@ def plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt):
         ## plot contours
 
 
-        ax = plt.subplot(numrows, PLOTS_PER_ROW, i+1)
+        ax = plt.subplot(numrows, PLOTS_PER_ROW, phase_index+1)
 
         best_ebv = d['BEST_EBV'] 
         minebv_1sig = d['EBV_1SIG'][0] 
-        maxebv_1sig = d['EBV_1SIG'][0]
+        maxebv_1sig = d['EBV_1SIG'][1]
 
         best_rv = d['BEST_RV'] 
         minrv_1sig = d['RV_1SIG'][0] 
-        maxrv_1sig = d['RV_1SIG'][0]
+        maxrv_1sig = d['RV_1SIG'][1]
         
         CDF = d['CDF']
         best_av = d['BEST_AV']
         sig_av = d['SIG_AV']
-
-#        sig_hi_ebv = np.array([d['EBV_1SIG'][1] - d['BEST_EBV'] for d in SN12CU_CHISQ_DATA])
-#        sig_ebv = np.array([d['SIG_EBV'] for d in SN12CU_CHISQ_DATA])
-
-
-
-#        rvs = np.array([d['BEST_RV'] for d in SN12CU_CHISQ_DATA])
-#        sig_lo_rv = np.array([d['BEST_RV'] - d['RV_1SIG'][0] for d in SN12CU_CHISQ_DATA])
-#        sig_hi_rv = np.array([d['RV_1SIG'][1] - d['BEST_RV'] for d in SN12CU_CHISQ_DATA])
-#        sig_rv = np.array([d['SIG_RV'] for d in SN12CU_CHISQ_DATA])
 
 
         best_av = d['BEST_AV']
@@ -123,20 +117,21 @@ def plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt):
         plt.scatter(best_ebv, best_rv, marker='s', facecolors='r')
         
         # show results on plot
-        if i%6==0:
+        if phase_index%6==0:
                 plttext1 = "Phase: {}".format(phase)
         else:
                 plttext1 = "{}".format(phase)
                 
-        plttext2 = "$E(B-V)={:.2f}\pm^{{{:.2f}}}_{{{:.2f}}}$" + \
-                   "\n$R_V={:.2f}\pm^{{{:.2f}}}_{{{:.2f}}}$" + \
-                   "\n$A_V={:.2f}\pm^{{{:.2f}}}_{{{:.2f}}}$"
-        plttext2 = plttext2.format(best_ebv, maxebv_1sig-best_ebv, best_ebv-minebv_1sig,
-                                   best_rv, maxrv_1sig-best_rv, best_rv-minrv_1sig,
-                                   best_av, sig_av, sig_av
+        plttext2 = "$E(B-V)={:.2f}^{{{:+.2f}}}_{{{:+.2f}}}$" + \
+                   "\n$R_V={:.2f}^{{{:+.2f}}}_{{{:+.2f}}}$" + \
+                   "\n$A_V={:.2f}^{{{:+.2f}}}_{{{:+.2f}}}$"
+        
+        plttext2 = plttext2.format(best_ebv, maxebv_1sig-best_ebv, minebv_1sig-best_ebv,
+                                   best_rv, maxrv_1sig-best_rv, minrv_1sig-best_rv,
+                                   best_av, sig_av, -sig_av
                                    )
                                            
-        if phase not in [11.5, 16.5, 18.5, 21.5]:
+        if phase not in [14.5, 16.5, 18.5, 21.5, 23.5]:
                 ax.text(.04, .95, plttext1, size=AXIS_LABEL_FONTSIZE,
                         horizontalalignment='left',
                         verticalalignment='top',
@@ -145,7 +140,7 @@ def plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt):
                         horizontalalignment='left',
                         verticalalignment='top',
                         transform=ax.transAxes)
-                ax.axhspan(2.9, (y.max()), facecolor='k', alpha=0.1)
+                #ax.axhspan(2.9, (y.max()), facecolor='k', alpha=0.1)  # this produces a gray band, which I'm not using.  12/23/14
         
         else:
                 ax.text(.04, .95, plttext1, size=AXIS_LABEL_FONTSIZE,
@@ -156,29 +151,34 @@ def plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt):
                         horizontalalignment='left',
                         verticalalignment='top',
                         transform=ax.transAxes)
-                ax.axhspan(3.32, (y.max()), facecolor='k', alpha=0.1)
-                ax.axhspan((y.min()), 2.5, facecolor='k', alpha=0.1)
+                #ax.axhspan(3.32, (y.max()), facecolor='k', alpha=0.1)
+                #ax.axhspan((y.min()), 2.5, facecolor='k', alpha=0.1)
                 
                 
         # format subplot...
         plt.ylim(y.min(), y.max())
-        plt.xlim(x.min(), y.max())
+        plt.xlim(x.min(), x.max())
         
         ax.set_yticklabels([])
         ax2 = ax.twinx()
         ax2.set_xlim(x.min(), x.max())
         ax2.set_ylim(y.min(), y.max())
         
-        if i%6 == 5:
+        if phase_index%6 == 5:
                 ax2.set_ylabel('\n$R_V$', fontsize=AXIS_LABEL_FONTSIZE, labelpad=5)
-        if i%6 == 0:
+        if phase_index%6 == 0:
                 ax.set_ylabel('$R_V$', fontsize=AXIS_LABEL_FONTSIZE, labelpad=-2)
-        if i>=6:
+        if phase_index>=5:
                 ax.set_xlabel('\n$E(B-V)$', fontsize=AXIS_LABEL_FONTSIZE)
         
-        # format x labels
+        ## format x labels
+        ax.locator_params(nbins=4)  # this sets the number of tickmarks at 5.
         labels = ax.get_xticks().tolist()
-        labels[0] = labels[-1] = ''
+        
+        # get rid of the first (0) and the last label (1.4); i.e. the labels at the ends.
+        labels[0] = labels[-1] = ''   
+
+        #exit(1)
         ax.set_xticklabels(labels)
         ax.get_xaxis().set_tick_params(direction='in', pad=-20)
         
@@ -194,7 +194,7 @@ def plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt):
 
 
     fig.subplots_adjust(left=0.04, bottom=0.08, right=0.95, top=0.92, hspace=.06, wspace=.1)
-    fig.suptitle('SN2012CU: $E(B-V)$ vs. $R_V$ Contour Plot per Phase', fontsize=TITLE_FONTSIZE)
+    #fig.suptitle('SN2012CU: $E(B-V)$ vs. $R_V$ Contour Plot per Phase', fontsize=TITLE_FONTSIZE)
 
     SNcontour = SNname + '_contour'
     
@@ -225,7 +225,7 @@ def plot_phase_excesses(SNname, SN12CU_CHISQ_DATA, red_law, unfilt, snake = True
     
     print "Plotting excesses of", SNname, " with best fit from contour..."
     
-    fig = plt.figure(figsize = (20, 12))
+    fig = plt.figure(figsize = (24, 15))
     
     #numrows = (len(EXCESS)-1)//PLOTS_PER_ROW + 1
     ## Keep; may need this later: pmin, pmax = np.min(phases), np.max(phases)
@@ -327,11 +327,27 @@ def plot_phase_excesses(SNname, SN12CU_CHISQ_DATA, red_law, unfilt, snake = True
 
 
 
-        ax.text(.95, .5, plttext, size=INPLOT_LEGEND_FONTSIZE,
+        ax.text(.95, .45, plttext, size=INPLOT_LEGEND_FONTSIZE,
                 horizontalalignment='right',
                 verticalalignment='top',
                 transform=ax.transAxes)
+   
+
+
+#        ## Alternative way of labeling phases -- with text, rather than subplot title.
+#        if phase_index%6==0:
+#                plttext1 = "Phase: {}".format(phase)
+#        else:
+#                plttext1 = "{}".format(phase)
+#                
+#                                           
+#        ax.text(.04, .95, plttext1, size=AXIS_LABEL_FONTSIZE,
+#                horizontalalignment='left',
+#                verticalalignment='top',
+#                transform=ax.transAxes)
         
+
+   
         ## format subplot
         if phase_index%PLOTS_PER_ROW == 0:
             ax.set_title('Phase: {}'.format(phase), fontsize=AXIS_LABEL_FONTSIZE)
@@ -346,12 +362,20 @@ def plot_phase_excesses(SNname, SN12CU_CHISQ_DATA, red_law, unfilt, snake = True
         labels[0] = labels[-1] = ''
         ax.set_yticklabels(labels)
         
+        ax.locator_params(axis = 'x', nbins=4)  # this sets the number of tickmarks at 5.
         labels = ax.get_xticks().tolist()
+        labels = [int(label) for label in labels]
         labels[0] = labels[-1] = ''
+        major_formatter = FormatStrFormatter('%4.0f')
+        ax.xaxis.set_major_formatter(major_formatter)
         ax.set_xticklabels(labels)
         
         plt.setp(ax.get_xticklabels(), fontsize=TICK_LABEL_FONTSIZE)
         plt.setp(ax.get_yticklabels(), fontsize=TICK_LABEL_FONTSIZE)
+
+        ## Trimming margins so that more space is used for the plots, and controls space between subplots.
+        fig.subplots_adjust(left=0.04, bottom=0.08, right=0.95, top=0.92, hspace=.12, wspace=.16)
+
 
 
     SNsummary = SNname + '_excess'
@@ -453,7 +477,7 @@ def plot_summary(SNname, SN12CU_CHISQ_DATA, unfilt):
                             horizontalalignment='left', \
                             verticalalignment='top', \
                             transform=ax.transAxes)
-    plt.ylabel('$E(B-V)$')
+    plt.ylabel('$E(B-V)$', fontsize=AXIS_LABEL_FONTSIZE)
 
 
     ax = fig.add_subplot(412) 
@@ -466,7 +490,7 @@ def plot_summary(SNname, SN12CU_CHISQ_DATA, unfilt):
                             horizontalalignment='left', \
                             verticalalignment='top', \
                             transform=ax.transAxes)
-    plt.ylabel('$R_V$')
+    plt.ylabel('$R_V$', fontsize=AXIS_LABEL_FONTSIZE)
 
 
     ax = fig.add_subplot(413) 
@@ -479,7 +503,7 @@ def plot_summary(SNname, SN12CU_CHISQ_DATA, unfilt):
                             horizontalalignment='left', \
                             verticalalignment='top', \
                             transform=ax.transAxes)
-    plt.ylabel('$A_V$')
+    plt.ylabel('$A_V$', fontsize=AXIS_LABEL_FONTSIZE)
 
     
     ax = fig.add_subplot(414) 
@@ -492,11 +516,11 @@ def plot_summary(SNname, SN12CU_CHISQ_DATA, unfilt):
                             horizontalalignment='left', \
                             verticalalignment='top', \
                             transform=ax.transAxes)
-    plt.ylabel('$\Delta\mu$')
+    plt.ylabel('$\Delta\mu$', fontsize=AXIS_LABEL_FONTSIZE)
 
 
 
-    plt.xlabel('Phases')
+    plt.xlabel('Phases', fontsize=AXIS_LABEL_FONTSIZE)
     
     SNsummary = SNname + '_summary'
     
@@ -536,4 +560,4 @@ if __name__ == "__main__":
 
     plot_contours(SNname, SN12CU_CHISQ_DATA, unfilt)
     plot_summary(SNname, SN12CU_CHISQ_DATA, unfilt)
-    plot_phase_excesses('SN2012CU', SN12CU_CHISQ_DATA, redden_fm, unfilt, snake = True)
+    plot_phase_excesses(SNname, SN12CU_CHISQ_DATA, redden_fm, unfilt, snake = True)
